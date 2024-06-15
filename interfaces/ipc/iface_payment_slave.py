2d35f93c84c53b3c8279248456fe5431960b61ed
zaebalkin <g@spekhov.ru>
2024-06-15 14:15:43 +0400

    very-very important update

--- interfaces/ipc/iface_payment_slave.py
+++ interfaces/ipc/iface_payment_slave.py
@@ -8,26 +8,26 @@ class ifacePAYMENTslave(InterfaceCommon):
 
 		print ('PS: %s'%self['payment_method'])
 
-		with self.lock:
-			if not (group in self['payment_method']):
-				self['payment_method'][group]=dict()
-			if not (name in self['payment_method'][group]):
-				self['payment_method'][group][name]=dict(
-						name=name,
-						group=group,
-						cashless=cashless,
-						remote=remote,
-						coupon=coupon,
-						bank=bank,
-						without_known_price=without_known_price,
-						status='DISCONNECTED',
-						can_be_used=False,
-						is_ready=False,
-						is_enabled=False,
-						fixed_nominals=False,
-					)
-			else:
-				raise AlreadyExists('%s-%s payment_method alreary registered, %s'%(group,name,self['payment_method']))
+		#with self.lock:
+		if not (group in self['payment_method']):
+			self['payment_method'][group]=dict()
+		if not (name in self['payment_method'][group]):
+			self['payment_method'][group][name]=dict(
+					name=name,
+					group=group,
+					cashless=cashless,
+					remote=remote,
+					coupon=coupon,
+					bank=bank,
+					without_known_price=without_known_price,
+					status='DISCONNECTED',
+					can_be_used=False,
+					is_ready=False,
+					is_enabled=False,
+					fixed_nominals=False,
+				)
+		else:
+			raise AlreadyExists('%s-%s payment_method alreary registered, %s'%(group,name,self['payment_method']))
 
 		self.able=self['payment_method'][group][name]
 
