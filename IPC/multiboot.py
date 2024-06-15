25b1083a7db233d88b5b556c9e9a2ffc0ec55693
zaebalkin <g@spekhov.ru>
2024-06-15 13:43:42 +0400

    very-very important update

--- IPC/multiboot.py
+++ IPC/multiboot.py
@@ -12,20 +12,16 @@ _dsi=DictProxy.__setitem__
 
 
 def set_proc_name(newname):
-    newname=newname.encode()
-    from ctypes import cdll, byref, create_string_buffer
-    libc = cdll.LoadLibrary('libc.so.6')
-    buff = create_string_buffer(len(newname)+1)
-    buff.value = newname
-    libc.prctl(15, byref(buff), 0, 0, 0)
+    try:
+        newname=newname.encode()
+        from ctypes import cdll, byref, create_string_buffer
+        libc = cdll.LoadLibrary('libc.so.6')
+        buff = create_string_buffer(len(newname)+1)
+        buff.value = newname
+        libc.prctl(15, byref(buff), 0, 0, 0)
+    except:
+        pass
 
-def get_proc_name():
-    from ctypes import cdll, byref, create_string_buffer
-    libc = cdll.LoadLibrary('libc.so.6')
-    buff = create_string_buffer(128)
-    # 16 == PR_GET_NAME from <linux/prctl.h>
-    libc.prctl(16, byref(buff), 0, 0, 0)
-    return buff.value
 
 def list__repr__(self):
         dv=''
