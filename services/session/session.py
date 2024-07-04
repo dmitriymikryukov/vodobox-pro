26822f485ccc330780346f160668046bc128b152
zaebalkin <g@spekhov.ru>
2024-07-04 14:22:05 +0400

    very-very important update

--- services/session/session.py
+++ services/session/session.py
@@ -72,7 +72,7 @@ class SgnSession(sgnService):
 
 	@subscribe
 	def EventMoneyStacked(self,amount,mtype):
-		self.debug('Сессия: Пополение баланса на %s через %s'%(self.nominal_to_text_with_currency(amount),mtype))
+		self.debug('Сессия: Пополение баланса на %s через %s'%(amount,mtype))
 		if mtype in ['CASH']:
 			self['session']['cash_balance']+=amount
 
