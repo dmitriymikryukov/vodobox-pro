8a53cb93457c2af50f5e5165cafe28072f9283b2
zaebalkin <g@spekhov.ru>
2024-07-14 14:42:18 +0400

    very-very important update

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export display=:0
+
 ps aux  |  grep -i ./boot.py  |  awk '{print $2}'  |  xargs sudo kill -9
 python3 ./boot.py
 ps aux  |  grep -i ./boot.py  |  awk '{print $2}'  |  xargs sudo kill -9
