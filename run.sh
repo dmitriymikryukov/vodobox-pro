24543d7017bb82189f72eab01d5fab005fe722a9
zaebalkin <g@spekhov.ru>
2024-07-20 13:12:11 +0400

    very-very important update

--- run.sh
+++ run.sh
@@ -7,3 +7,4 @@ export DISPLAY=:0
 ps aux  |  grep -i ./boot.py  |  awk '{print $2}'  |  xargs sudo kill -9
 python3 ./boot.py
 ps aux  |  grep -i ./boot.py  |  awk '{print $2}'  |  xargs sudo kill -9
+ps aux  |  grep -i ./vendor/guiboard.py  |  awk '{print $2}'  |  xargs sudo kill -9
