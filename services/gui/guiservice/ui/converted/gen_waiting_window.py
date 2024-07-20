7e9c4d60f59bfec80d86dfb4e935c1e090029d0d
unknown <bl4ckfl4me@yandex.ru>
2024-07-20 16:21:46 +0400

    testing wating window

--- services/gui/guiservice/ui/converted/gen_waiting_window.py
+++ services/gui/guiservice/ui/converted/gen_waiting_window.py
@@ -14,8 +14,8 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(800, 530)
-        Form.setMaximumSize(QtCore.QSize(1024, 600))
+        Form.resize(800, 600)
+        Form.setMaximumSize(QtCore.QSize(800, 600))
         self.verticalLayout = QtWidgets.QVBoxLayout(Form)
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout.setSpacing(0)
@@ -47,7 +47,6 @@ class Ui_Form(object):
         sizePolicy.setHeightForWidth(self.frame_17.sizePolicy().hasHeightForWidth())
         self.frame_17.setSizePolicy(sizePolicy)
         self.frame_17.setMinimumSize(QtCore.QSize(0, 129))
-        self.frame_17.setMaximumSize(QtCore.QSize(16777215, 149))
         self.frame_17.setStyleSheet("#frame_17 {\n"
 "    background-color: white;\n"
 "    border-radius: 20px;\n"
