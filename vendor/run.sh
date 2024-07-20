69cee780bce4b8defe1805df33ec8052c458a3ed
zaebalkin <g@spekhov.ru>
2024-07-20 13:02:43 +0400

    very-very important update

---
+++ vendor/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export DISPLAY=:0
+
+ps aux  |  grep -i ./vendor/guiboard.py  |  awk '{print $2}'  |  xargs sudo kill -9
+sudo python3 ./vendor/guiboard.py &
+ps aux  |  grep -i ./vendor/guiboard.py  |  awk '{print $2}'  |  xargs sudo kill -9
