a1b45f805b2ae8ddbd80d248340ee8f8523b7e6c
zaebalkin <g@spekhov.ru>
2024-09-07 14:48:23 +0400

    very-very important update

--- vendor/flowmeter/flowtest.py
+++ vendor/flowmeter/flowtest.py
@@ -65,7 +65,6 @@ def flow(vol,pls):
             for name, line in merge_pipes(out=p.stdout, err=p.stderr):
                 print("FLOW SAYS: %s:%s"%(name,line))
 
-            status = p.wait()
         finally:
             try:
                 #p.send_signal(signal.SIGINT)
@@ -73,9 +72,12 @@ def flow(vol,pls):
             except:
                 os.kill(p.pid,signal.SIGKILL)
                 #p.send_signal(signal.SIGKILL)            
+            status = p.wait()
+            print("DONE: STATUS: %s"%status)
     ppp=threading.Thread(target=xyu,args=[cmd],daemon=True)
     ppp.start()
     time.sleep(5)
+    print("DEMO KILL")
     procc.send_signal(signal.SIGINT)
     #os.kill(p.pid,signal.SIGINT)
     #os.system("kill -SIGINT %s &"%p.pid)
