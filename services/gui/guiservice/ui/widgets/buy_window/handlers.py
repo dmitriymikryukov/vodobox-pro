11f41fc5c1bf7c3bcf870a7b430318771cd5d8c9
bl4ckfl4me-dev <bl4ckfl4me-dev@yandex.ru>
2024-09-14 10:57:51 +0400

    bug fix with run_flow

--- services/gui/guiservice/ui/widgets/buy_window/handlers.py
+++ services/gui/guiservice/ui/widgets/buy_window/handlers.py
@@ -7,8 +7,7 @@ def run_flow(volume_ml, pls):
     global process
     # Запускаем процесс
     process = subprocess.Popen(
-        f'/opt/kiosk/vodobox-pro/vendor/flowmeter/flow {volume_ml} {pls}'
-        ['python', 'your_script.py'],  # Замените на ваш скрипт
+        f'/opt/kiosk/vodobox-pro/vendor/flowmeter/flow {volume_ml} {pls}',
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         text=True,
